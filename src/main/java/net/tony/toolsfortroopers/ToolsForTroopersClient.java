6bb0d9519c984a7d5572e4f7c08b433d740259dd
Anthony Smith <powerofthewolfie@gmail.com>
2025-06-25 15:08:08 -0400

    Initial commit

---
+++ src/main/java/net/tony/toolsfortroopers/ToolsForTroopersClient.java
@@ -0,0 +1,10 @@
+package net.tony.toolsfortroopers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToolsForTroopersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tony/toolsfortroopers/ToolsForTroopersDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tony.toolsfortroopers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ToolsForTroopersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
