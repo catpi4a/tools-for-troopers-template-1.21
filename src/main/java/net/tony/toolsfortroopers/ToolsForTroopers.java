985a4ce52aa4ca06d70946114f8a15ac12efd465
Anthony Smith <powerofthewolfie@gmail.com>
2025-06-26 20:27:15 -0400

    adds recipe for smelting raw titanium

--- src/main/java/net/tony/toolsfortroopers/ToolsForTroopers.java
+++ src/main/java/net/tony/toolsfortroopers/ToolsForTroopers.java
@@ -3,6 +3,7 @@ package net.tony.toolsfortroopers;
 import net.fabricmc.api.ModInitializer;
 
 import net.tony.toolsfortroopers.block.ModBlocks;
+import net.tony.toolsfortroopers.item.ModItemGroups;
 import net.tony.toolsfortroopers.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,5 +17,7 @@ public class ToolsForTroopers implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		ModItemGroups.registerItemGroups();
 	}
 }
